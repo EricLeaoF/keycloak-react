f3c893db67ed6f3999b3c4d7a93a97bf21e6e7fd
eric.freitas <eric.freitas@icolabora.com>
2025-01-13 16:58:51 -0300

    changed section name

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -87,7 +87,7 @@ const Layout: React.FC = () => {
                         <Link to="/">
                           <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
                             <i className="pi pi-home mr-2"></i>
-                            <span className="font-medium">Dashboard</span>
+                            <span className="font-medium">Home</span>
                             <Ripple />
                           </a>
                         </Link>
